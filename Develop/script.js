b94163a632b8a8ff113dbccdddd7dcd3131b79b4
Ult Des <truttling.mariah@gmail.com>
2020-12-16 23:59:04 -0500

    Final updates

--- Develop/script.js
+++ Develop/script.js
@@ -1,100 +1,128 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  var passwordText=document.querySelector("#password");
+  passwordText.values=password;
+  
 }
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+// established variables for each character type in an array
 var charNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var charSpecial = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '"', ']', '[', '{', '}', '|', '/', '.', '<', '>', '?', ';', ':'];
 var charUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var charLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-function passCheck(){
-  var lenghtCheck=parseInt(prompt("Choose a Password Lenght Between 8 & 128"));
-  while (lenghtCheck<8 || lenghtCheck>128){
+
+// function to check password criteria of length, numbers, specials, upper and lower case letters. Will keep alerting until valid input.
+function passCheck() {
+
+  var lenghtCheck = parseInt(prompt("Choose a Password Lenght Between 8 & 128")); // parseInt used to convert length inputted to interger value
+  if (lenghtCheck < 8 || lenghtCheck > 128) {  //invalid parameters
     alert("Password Lenght is Invalid. Choose a Number Between 8 & 128!")
+    lenghtCheck = parseInt(prompt("Choose a Password Lenght Between 8 & 128"));
+
   }
-  if(lenghtCheck=""){
-    alert("You MUST enter a value!")
-  }
-  if (lenghtCheck<=8 || lenghtCheck<=128){
-    var numeriCon=confirm("Would you Like you Password to Contain Numbers?")
+
+  if (lenghtCheck >= 8 && lenghtCheck <= 128) { // valid intial length check then asked to confirm other password props
+    var numeriCon = confirm("Would you Like you Password to Contain Numbers?");
     var specialCon = confirm("Would You Like Special Characters in Password?");
-    var  upperCon = confirm("Would You Like Uppercase Letters in Password?");
-    var  lowerCon = confirm("Would You Like Lowercase Letters in Password?");
-  
+    var upperCon = confirm("Would You Like Uppercase Letters in Password?");
+    var lowerCon = confirm("Would You Like Lowercase Letters in Password?");
   }
-  if(!numeriCon && !specialCon && !upperCon && !lowerCon){
-    alert(" Please Choose at Least One Character Type!")
+
+
+
+  if (!numeriCon && !specialCon && !upperCon && !lowerCon) { //condition when no character type is selected.
+    alert(" Please Choose at Least One Character Type!");
+    var numeriCon = confirm("Would you Like you Password to Contain Numbers?");
+    var specialCon = confirm("Would You Like Special Characters in Password?");
+    var upperCon = confirm("Would You Like Uppercase Letters in Password?");
+    var lowerCon = confirm("Would You Like Lowercase Letters in Password?");
+
+
+
+
   }
+  var passCheckOpts = { //object made to house all password properties
+    lens: lenghtCheck,
+    number: numeriCon,
+    special: specialCon,
+    upperC: upperCon,
+    lowerC: lowerCon
+  }
+  return passCheckOpts;
+
+
+
+}
+// Generating password
+function generatePassword() { 
+
+  var choices = passCheck(); // to add/ change values for password props
+  console.log(choices)
+
+  var intPass = []; // props stored in this array each iteration. initial password array
+  console.log(intPass)
+
+
+
+  if (choices.number) { //pushing number prop to intPass
+    for (i = 0; i < charNum.length; i++) {
+      intPass.push(charNum[i]);
+    }
+
+  }
+  if (choices.special) { // pushing special prop to intPass
+    for (i = 0; i < charSpecial.length; i++) {
+      intPass.push(charSpecial[i]);
+    }
+  }
+  if (choices.upperC) { //pushing upperC prop to intPass
+    for (i = 0; i < charUpper.length; i++) {
+      intPass.push(charUpper[i]);
+    }
+  }
+  if (choices.lowerC) { //pushing lowerC prop into intPass
+    for (i = 0; i < charLower.length; i++) { 
+      intPass.push(charLower[i]);
+    }
+  }
+
+
+  var finPass = []; //final password array
+
+  for (let i = 0; i < choices.lens; i++) {
+    var randoPass = Math.floor(Math.random() * Math.floor(intPass.length)); // Math floor rounds to nearest whole number, math random gives random values 
+    //that will make up intPass
+    finPass.push(intPass[randoPass]) //appending intPass and randoPass array vars to final password array
+  }
+
+  console.log(finPass); //outputs array of values
+
+  password = finPass.join(''); //combines all array elements into single string
+  console.log(password);
+
+
 
 }
 
 
-   /* var criteria= {
-    lenght: clickButton,
-    numeric: numericCon,
-    special: specialCon,
-    upper: upperCon,
-    lower: lowerCon
-    }
-    return criteria;
-  }
-  
-  
-  function passwordGen() {
-    var password = generatePassword();
-    console.log(password);
-    
-    var passChoices = [];
-    console.log(passChoices)
-  if (password.numeric) {
-    for (i = 0; i < charNum.length; i++) {
-      passChoices.push(charNum[i]);
 
-    }
-  }
-  if (password.special) {
-    for (i = 0; i < charSpecial.length; i++) {
-      passChoices.push(charSpecial[i]);
-    }
-  }
 
-  if (password.upper) {
-    for (i = 0; i < charUpper.length; i++) {
-      passChoices.push(charUpper[i]);
-    }
-  }
-  if (password.lower) {
-    for (i = 0; i < charLower.length; i++) {
-      passChoices.push(charLower[i]);
-    }
-  }
 
-  var passwordCom=[];
-  for (i=0; i<password.lenght; i++){
-      var randomPass=Math.floor((Math.random() * Math.floor(passChoices.lenght)));
-      passwordCom.push(passChoices [randomPass]);
-      console.log(passwordCom);
 
-  }
-  
-    var pp=passwordCom.join("");
-    console.log(pp);
-   pp = document.querySelector("#password").textContent;
-    
 
-  }
-  
-  
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); */
+
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Password_Generator
+>This project showcases the using of Javascript to create a password generator. When the button is clicked, the user is prompted to input the desired password length. If it is between the the outlined parameters, the user is then asked further which character types-- numbers,special,upper case, and lower case-- they would like in their password. The password will then appear in the text box.
+
+> Honestly, it was a tough project because there are many ways to go about doing this. But overall, I gained a better understanding of the use of functions,objects,loops,and logic.
+
+## Live Url
+[PASSWORD GENERATOR]()
+
+> Honestly, it was a tough project because there are many ways to go about doing this. But overall, I gained a better understanding of the use of functions,objects,loops,and logic.
+
+[Password Generator Screenshot](03-javascript-homework-demo.png)
+
+[Password Generator Output](Capture.PNG)
+
+## The Process
+* Defined variables for input of length,special characters,numeric characters,uppercase and lowercase characters
+* Created a function to check for valid password criteria and stored into object
+* Created function to generate the actual password
+* Used condition statements for validation
+* Used for loops to iterate through variable arrays made
+
+## Programs
+* Visual Studio Code
+* Git Bash
+
+
+## Resources Used
+[W3Schools](https://www.w3schools.com/)
+[MDN](https://developer.mozilla.org/en-US/)
+[Youtube]
+>Javascript & JQuery: Interactive Front-End Web Development, Jon Duckett
+
+
